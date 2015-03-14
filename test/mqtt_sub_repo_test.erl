7f6a0ace2312ba4786406794ff399b08cc706c17
Kalin <kalin.gyokov>
2015-03-14 11:33:58 -0700

    Test cleanup

--- test/mqtt_sub_repo_test.erl
+++ test/mqtt_sub_repo_test.erl
@@ -80,28 +80,15 @@ my_test_() ->
                             ?lists_are_equal([{<<"Client1">>,1},{<<"Client2">>,2}], mqtt_sub_repo:get_all(<<"/A/1">>))
                         end
                     },
-
-
-                    {spawn,
-                        fun() ->
-                            mqtt_sub_repo:add_sub(<<"Client1">>, <<"/A/+">>, 1),
-                            mqtt_sub_repo:remove_sub(<<"Client1">>, <<"/A/+">>),
-                            ?lists_are_equal([], mqtt_sub_repo:get_all(<<"/A/1">>))
-                        end
-                    },
-
-
                     {spawn,
                         fun() ->
                             mqtt_sub_repo:add_sub(<<"Client1">>, <<"/A/+">>, 1),
                             mqtt_sub_repo:remove_sub(<<"Client1">>, <<"/A/+">>),
 
-                            %%mqtt_sub_repo:add_sub(<<"Client2">>, <<"/+/1">>, 2),
-                            ?lists_are_equal([], mqtt_sub_repo:get_all(<<"/A/1">>))
-%%                             ?lists_are_equal([{<<"Client2">>,2}], mqtt_sub_repo:get_all(<<"/A/1">>))
+                            mqtt_sub_repo:add_sub(<<"Client2">>, <<"/+/1">>, 2),
+                            ?lists_are_equal([{<<"Client2">>,2}], mqtt_sub_repo:get_all(<<"/A/1">>))
                         end
                     },
-
                     {spawn,
                         fun() ->
                             mqtt_sub_repo:add_sub(<<"Client1">>, <<"/A/+">>, 1),
