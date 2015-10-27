a779191341118dc3613dcba30e957acf5fc36daa
Kalin <kalin.gyokov>
2015-10-26 22:53:29 -0700

    Refactoring

--- src/mqtt_router.erl
+++ src/mqtt_router.erl
@@ -18,40 +18,52 @@
 
 global_route(Msg = #mqtt_message{topic = Topic,qos = MsgQoS,
                                  content = Content,seq = Seq}) ->
-
     mqtt_topic_repo:enqueue(Topic,Msg),
-    Subs = mqtt_sub_repo:get_matches(Topic),
+    Regs = get_client_regs(Topic),
 
-    CTRPacket = {Topic,Content,Seq},
-    Regs = lists:filtermap(fun({ClientId,SubQoS}) ->
-                QoS = min(MsgQoS,SubQoS),
-                case mqtt_reg_repo:get_registration(ClientId) of
-                    {ok,Pid}  ->
-                        {true,{live,{Pid,ClientId},QoS}};
-                    undefined when QoS =/= ?QOS_0 ->
-                        {true,{dead, ClientId, QoS}};
-                    undefined when QoS =:= ?QOS_0 ->
-                        false
-                end
-         end,Subs),
+    {QoS_0,MaybeLive,Dead} = get_regs_by_state(Regs,MsgQoS),
+
     error_logger:info_msg("To enqueue ~p for topic ~p",[Msg,Topic]),
-    {QoS_0,QoS_Reliable} = lists:partition(fun({_,_,QoS}) -> QoS =:= ?QOS_0 end,Regs),
-    {MaybeLive,Dead} = lists:partition(fun({State,_,_QoS}) -> State =:= live end, QoS_Reliable),
-
+    CTRPacket = {Topic,Content,Seq},
     % Send out QoS messages, do not wait for response
     [mqtt_session_out:push_qos0(Pid,CTRPacket) || {_,{Pid,_},_QoS} <- QoS_0],
     %% Send out QoS 1/2 messages to registered processes and wait for response
     SyncResults = rpc:pmap({?MODULE,fwd_message},[CTRPacket],MaybeLive),
+
+    %% Handle results
     StaleRegs = [Tuple || {noproc,Tuple} <- SyncResults],
     %% Get rid of any stale process registrations
     lists:map(fun({Pid,ClientId,_}) -> mqtt_reg_repo:unregister(Pid,ClientId) end,
               StaleRegs),
-    %% persist if necessary
+    %% persist if there are registrations
     case {StaleRegs,Dead} of
         {[],[]} -> ok;
         _       -> persist_message(CTRPacket)
     end.
 
+get_client_regs(Topic) ->
+    [begin
+        Reg = mqtt_reg_repo:get_registration(ClientId),
+        {ClientId,SubQoS,Reg}
+     end
+        || {ClientId,SubQoS} <- mqtt_sub_repo:get_matches(Topic)].
+
+get_regs_by_state(Regs,MsgQoS) ->
+    RegStates = lists:filtermap(fun({ClientId,SubQoS,Reg}) ->
+        QoS = min(MsgQoS,SubQoS),
+        case Reg of
+            {ok,Pid}  ->
+                {true,{live,{Pid,ClientId},QoS}};
+            undefined when QoS =/= ?QOS_0 ->
+                {true,{dead, ClientId, QoS}};
+            undefined when QoS =:= ?QOS_0 ->
+                false
+        end
+    end,Regs),
+
+    {QoS_0,QoS_Reliable} = lists:partition(fun({_,_,QoS}) -> QoS =:= ?QOS_0 end,RegStates),
+    {MaybeLive,Dead} = lists:partition(fun({State,_,_QoS}) -> State =:= live end, QoS_Reliable),
+    {QoS_0,MaybeLive,Dead}.
 
 fwd_message({live,{Pid,ClientId},QoS},CTRPacket) ->
     %% @todo: use basic messaging (instead of depending on the specifics of gen_server:call)
