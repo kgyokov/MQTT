86c874a1035aa2aa01302481e34a04d826426a75
Kalin <kalin.gyokov>
2016-01-22 22:41:57 -0800

    Finger tree measuring and concatentation

--- src/finger_tree.erl
+++ src/finger_tree.erl
@@ -3,24 +3,36 @@
 %%% @copyright (C) 2016, <COMPANY>
 %%% @doc
 %%% Based on Finger Trees (http://www.staff.city.ac.uk/~ross/papers/FingerTree.html)
-%%% A mostly direct translation to Erlang
+%%% Mostly direct translation to Erlang from the Haskell code in the paper
 %%% @end
 %%%-------------------------------------------------------------------
 -module(finger_tree).
 -author("Kalin").
 
 %% API
--export([pushl/2, pushr/2, is_empty/1, headl/1, taill/1]).
+-export([pushl/2, pushr/2, is_empty/1, headl/1, taill/1, concat/2, measure/2]).
 
 %%
 %% Monoid behavior
 %%
--callback identity(T) -> T.
--callback binary_op(T,T) -> T.
+%%-callback identity(T) -> T.
+%%-callback binary_op(T,T) -> T.
 
 -define(SUSP(Expr), fun() -> Expr end).
 -define(FORCE(Expr),Expr()).
 
+-type tree_node(E) ::
+{node2,E,E}
+|{node3,E,E}.
+
+-type finger_tree(E) ::
+    empty
+    |{single,E}
+    |{deep,[E],finger_tree(tree_node(E)),[E]}.
+
+
+-spec pushl(E,finger_tree(E)) -> finger_tree(E).
+-spec pushr(finger_tree(E),E) -> finger_tree(E).
 
 pushl(A,empty)                  ->  {single,A};
 pushl(A,{single,B})             ->  {deep,[A],empty,[B]};
@@ -36,6 +48,19 @@ pushr({deep,PR,M,SF},A)         ->  {deep,PR,M,SF++[A]}.
 %%
 %% Reducers
 %%
+
+-spec reducer(Fun,XS,Z) -> Z when
+    Fun :: fun((E,Z) -> Z),
+    XS ::
+        tree_node(E)
+        |finger_tree(E)
+        |[E].
+-spec reducel(Fun,Z,XS) -> Z when
+    Fun :: fun((Z,E) -> Z),
+    XS ::
+        tree_node(E)
+        |finger_tree(E)
+        |[E].
 
 reducer(Fun,{node2,A,B},Z)      -> Fun(A,Fun(B,Z));
 reducer(Fun,{node3,A,B,C},Z)    -> Fun(A,Fun(B,Fun(C,Z)));
@@ -56,7 +81,7 @@ reducel(Fun,Z,{node2,B,A})      -> Fun(Fun(Z,B),A);
 reducel(Fun,Z,{node3,C,B,A})    -> Fun(Fun(Fun(Z,C),B),A);
 
 reducel(_Fun,Z,empty)           -> Z;
-reducel(Fun,Z,{single,X})       -> Fun(X,Z);
+reducel(Fun,Z,{single,X})       -> Fun(Z,X);
 reducel(Fun,Z,{deep,PR,M,SF})   ->
     Fun1 = fun(A,B) -> reducel(Fun,A,B)  end,
     Fun2 = fun(A,B) -> reducel(Fun2,A,B) end,
@@ -70,13 +95,17 @@ reducel(Fun,Z,L) when is_list(L) ->
 %% Conversion
 %%
 
-to_tree(L) -> lists:foldl(fun pushr/2,empty,L).
+-spec to_tree([E]) -> finger_tree(E).
+-spec to_list(finger_tree(E)) -> [E].
+to_tree(L) when is_list(L) -> lists:foldl(fun pushr/2,empty,L).
 to_list(T) -> reducer(fun(H,T) -> [H|T] end,T,[]).
 
 
 %%
 %% Navigation
 %%
+
+%% @todo: lazy view???
 
 viewl(empty)                -> nil;
 viewl({single,A})           -> {A,nil};
@@ -91,18 +120,82 @@ deepl([],M,SF)  ->
 deepl(PR,M,SF)  ->
     {deep,PR,M,SF}.
 
+-spec headl(finger_tree(E)) -> E.
 headl(T) ->
     {H,_} = viewl(T),
     H.
 
+-spec taill(finger_tree(E)) -> finger_tree(E).
 taill(T) ->
     {_,T} = viewl(T),
     T.
 
+-spec is_empty(finger_tree(E)) -> true|false.
 is_empty(T) ->
     case viewl(T) of
         nil     -> true;
         {_,_}   -> false
     end.
 
+
+%%
+%% Appending
+%%
+
+%% @todo: reducer and pushl???
+
+-spec app3(finger_tree(E),[E],finger_tree(E)) -> finger_tree(E).
+
+app3(empty,TS,XS)           -> reducer(fun pushl/2,TS,XS); %% @todo: optimize???
+app3(XS,TS,empty)           -> reducel(fun pushr/2,XS,TS);
+app3({single,X},TS,XS)      -> pushl(X,reducer(fun pushl/2,TS,XS));
+app3(XS,TS,{single,X})      -> pushr(reducel(fun pushr/2,XS,TS),X);
+
+app3({deep,PR1,M1,SF1},TS,{deep,PR2,M2,SF2}) ->
+    M3 = app3(M1,to_nodes(SF1++TS++PR2),M2),
+    {deep,PR1,M3,SF2}.
+
+-spec to_nodes([E]) -> [tree_node(E)].
+to_nodes([A,B])        -> [{node2,A,B}];
+to_nodes([A,B,C])      -> [{node3,A,B,C}];
+to_nodes([A,B,C,D])    -> [{node2,A,B},{node2,C,D}];
+to_nodes([A,B,C|T])    -> [{node3,A,B,C}|to_nodes(T)]. %% @todo: optimize???
+
+
+concat(XS,TS) -> app3(XS,[],TS).
+
+%%
+%% Measuring
+%%
+
+%%
+%% This might work much better with Elixir protocols, because we can define the Monoid {Id,As} using a protocol
+%%
+
+node2(A,B,Mo = {_,As,_}) ->
+    V = measure(A,Mo),
+    V1 = measure(B,Mo),
+    {node2,As(V,V1),A,B}.
+
+node3(A,B,C,Mo = {_,As,_})  ->
+    V = measure(A,Mo),
+    V1 = measure(B,Mo),
+    V2 = measure(C,Mo),
+    V3 = As(As(V,V1),V2),
+    {node3,V3,A,B,C}.
+
+deep(PR,M,SF,Mo = {_,As,_}) ->
+    V = measure(PR,Mo),
+    V1 = measure(M,Mo),
+    V2 = measure(SF,Mo),
+    V3 = As(As(V,V1),V2),
+    {deep,V3,PR,M,SF}.
+
+measure({node2,V,_,_},_Mo)          -> V;
+measure({node3,V,_,_,_},_Mo)        -> V;
+measure(D,{Id,As,M}) when is_list(D)-> reducel(fun(Z,E) -> As(Z,M(E)) end,Id,D);
+measure(empty,{Id,_,_})             -> Id;
+measure({single,X},Mo)              -> measure(X,Mo);
+measure({deep,V,_,_,_},_Mo)         -> V;
+measure(X,{_,_,M})                  -> M(X).
 
