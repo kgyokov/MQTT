09320a4a6658ec526c8ef26eb7a0f28cad34d966
Kalin <kalin.gyokov>
2016-03-04 20:47:26 -0800

    min_val_tree: Adding split functionality, guaranteeing monotonicity of values

--- src/min_val_tree.erl
+++ src/min_val_tree.erl
@@ -11,47 +11,92 @@
 -author("Kalin").
 
 %% API
--export([store/3, get_min/1, remove/2, new/0, get_val/2, is_empty/1]).
+-export([store/3, min/1, split/2, remove/2, new/0, new/1, get_val/2, is_empty/1]).
 
+new() -> new(fun(A,B) -> A =< B end).
 
-new() ->
+new(Compare) ->
     {
         gb_trees:empty(),   %% A tree of Vals. Each Val can have several keys associated with it
-        dict:new()    %% A tree of Keys, Every key has one corresponding Val associated with it
+        dict:new(),         %% A tree of Keys, Every key has one corresponding Val associated with it,
+        Compare             %% A function to compare the Vals (true if A =< B)
     }.
 
-get_val(Key,{_,ByKey}) ->
+%% @doc
+%% Takes the Num smallest elements from the data structure and returns them plus the remainder
+%% @end
+split(Num,Tree) ->
+    case is_empty(Tree) of
+        true -> {[],Tree};
+        false ->
+            {Acc,Tree1} = split(Num,Tree,[]),
+            {lists:reverse(Acc),Tree1}
+    end.
+
+
+split(Num,T,Acc) when Num =<0 -> {Acc,T};
+split(Num,T,Acc) ->
+    case view(T) of
+        nil -> {Acc,T};
+        {H,T1} -> split(Num-1,T1,[H|Acc])
+    end.
+
+
+view({ByVal,ByKey,Comp}) ->
+    case gb_trees:is_empty(ByVal) of
+        true ->
+            nil;
+        false ->
+            %% @todo: better way to remove the head
+            %% find the key corresponding to the head
+            {Val,Keys} = gb_trees:smallest(ByVal),
+            [HKey|_] = gb_sets:to_list(Keys),
+            {{HKey,Val},
+                %% remove the key corresponding to the head
+                {delete_key_from_val_idx(HKey,Val,ByVal),
+                 dict:erase(HKey,ByKey),
+                 Comp}}
+    end.
+
+get_val(Key,{_,ByKey,_}) ->
     case dict:find(Key,ByKey) of
         error -> none;
         {ok,Val} -> {ok,Val}
     end.
 
-remove(Key,{ByVal,ByKey}) ->
+remove(Key,T = {ByVal,ByKey,Comp}) ->
     case dict:find(Key,ByKey) of
         error ->
-            {ByVal,ByKey};
+            T;
         {ok,Val} ->
             {delete_key_from_val_idx(Key,Val,ByVal),
-             dict:erase(Key,ByKey)}
+             dict:erase(Key,ByKey),
+             Comp}
     end.
 
-store(Key,Val,{ByVal,ByKey}) ->
+store(Key,Val,T = {ByVal,ByKey,Comp}) ->
     case dict:find(Key,ByKey) of
         error ->
-            insert(Key,Val,{ByVal,ByKey});
-        {ok,Val} ->
-            {ByVal,ByKey};
-        {ok,OldVal} ->
-            ByKey1 = dict:store(Key,Val,ByKey),
-            ByVal1 = delete_key_from_val_idx(Key,OldVal,ByVal),
-            ByVal2 = add_key_to_val_idx(Key,Val,ByVal1),
-            {ByKey1,ByVal2}
+            insert(Key,Val,T);
+        {ok,OldVal}  ->
+            case Comp(OldVal,Val) of
+                true ->
+                    ByKey1 = dict:store(Key,Val,ByKey),
+                    ByVal1 = delete_key_from_val_idx(Key,OldVal,ByVal),
+                    ByVal2 = add_key_to_val_idx(Key,Val,ByVal1),
+                    {ByKey1,ByVal2,Comp};
+                _ ->
+                    T %% maintain monotonicity of Val - it can only increase
+            end
     end.
 
-is_empty({ByVal,_ByKey}) ->
+is_empty({ByVal,_ByKey,_Comp}) ->
     gb_trees:is_empty(ByVal).
 
-get_min({ByVal,_ByKey}) ->
+%% @doc
+%% Gets the smallest value in the tree (may correspond to multiple keys, therefore we only return the Val)
+%% @end
+min({ByVal,_ByKey,_Comp}) ->
     case gb_trees:is_empty(ByVal) of
         true ->
             none;
@@ -65,7 +110,7 @@ get_min({ByVal,_ByKey}) ->
 %% PRIVATE
 %% ===========================================================================================
 
-insert(Key,Val,{ByVal,ByKey}) ->
+insert(Key,Val,{ByVal,ByKey,Comp}) ->
     ByKey1 = dict:store(Key,Val,ByKey),
     ByVal1 =
         case gb_trees:is_defined(Val, ByVal) of
@@ -75,7 +120,7 @@ insert(Key,Val,{ByVal,ByKey}) ->
                 Keys = gb_sets:insert(Key,gb_sets:new()),
                 gb_trees:insert(Val,Keys, ByVal)
         end,
-    {ByVal1,ByKey1}.
+    {ByVal1,ByKey1,Comp}.
 
 
 delete_key_from_val_idx(Key,Val,ByVal) ->
--- test/min_val_tree_test.erl
+++ test/min_val_tree_test.erl
@@ -24,7 +24,12 @@ all_test_() ->
             fun min_is_updated_after_removing_all_keys_for_old_min/0,
             fun min_stays_same_if_not_all_keys_removed/0,
             fun min_stays_same_if_not_all_keys_removed/0,
-            fun min_stays_same_if_inrelated_key_removed/0
+            fun min_stays_same_if_inrelated_key_removed/0,
+            fun cannot_update_w_lower_value/0,
+            fun split_empty_tree/0,
+            fun split_tree_smaller_than_num/0,
+            fun split_tree_w_zero/0,
+            fun split_tree_general_case/0
         ].
 
 min_is_correct_after_several_inserts() ->
@@ -54,13 +59,13 @@ min_is_none_after_removal_of_all_elements() ->
     ]),
     ToRemove = [c,a,b,c],
     T1 = remove_keys(ToRemove,T),
-    ?assertEqual(min_val_tree:get_min(T1),none).
+    ?assertEqual(min_val_tree:min(T1),none).
 
 removal_on_empty_does_not_error() ->
     T = min_val_tree:new(),
     ToRemove = [a,b,c],
     T1 = remove_keys(ToRemove,T),
-    ?assertEqual(none, min_val_tree:get_min(T1)).
+    ?assertEqual(none, min_val_tree:min(T1)).
 
 min_is_updated_after_removing_all_keys_for_old_min() ->
     Pairs =
@@ -74,7 +79,7 @@ min_is_updated_after_removing_all_keys_for_old_min() ->
     T = store_pairs(Pairs),
     ToRemove = [n,y],
     T1 = remove_keys(ToRemove,T),
-    ?assertEqual(min_val_tree:get_min(T1),{ok,3}).
+    ?assertEqual(min_val_tree:min(T1),{ok,3}).
 
 min_stays_same_if_not_all_keys_removed() ->
     Pairs =
@@ -88,10 +93,9 @@ min_stays_same_if_not_all_keys_removed() ->
     T = store_pairs(Pairs),
     ToRemove = [n],
     T1 = remove_keys(ToRemove,T),
-    ?assertEqual(min_val_tree:get_min(T1),{ok,1}).
+    ?assertEqual(min_val_tree:min(T1),{ok,1}).
 
 min_stays_same_if_inrelated_key_removed() ->
-    min_val_tree:new(),
     Pairs =
         [
             {a,5},
@@ -102,7 +106,63 @@ min_stays_same_if_inrelated_key_removed() ->
     T1 = store_pairs(Pairs),
     ToRemove = [b],
     T2 =remove_keys(ToRemove,T1),
-    ?assertEqual(min_val_tree:get_min(T2),{ok,1}).
+    ?assertEqual(min_val_tree:min(T2),{ok,1}).
+
+
+cannot_update_w_lower_value() ->
+    Pairs =
+        [
+            {a,5},
+            {b,3},
+            {c,7}
+        ],
+    T1 = store_pairs(Pairs),
+    T2 = min_val_tree:store(a,1,T1),
+    ?assertEqual(min_val_tree:min(T2),{ok,3}),
+    ?assertEqual(min_val_tree:get_val(a,T2),{ok,5}).
+
+split_empty_tree() ->
+    T1 = min_val_tree:new(),
+    {L,T2} = min_val_tree:split(2,T1),
+    ?assertEqual(length(L),0),
+    ?assert(min_val_tree:is_empty(T2)).
+
+split_tree_smaller_than_num() ->
+    Pairs =
+        [
+            {a,5},
+            {b,3},
+            {c,7}
+        ],
+    T1 = store_pairs(Pairs),
+    {L,T2} = min_val_tree:split(5,T1),
+    ?assertEqual(3,length(L)),
+    ?assert( min_val_tree:is_empty(T2)).
+
+split_tree_w_zero() ->
+    Pairs =
+        [
+            {a,5},
+            {b,3},
+            {c,7}
+        ],
+    T1 = store_pairs(Pairs),
+    {L,T2} = min_val_tree:split(0,T1),
+    ?assertEqual(0,length(L)),
+    ?assert(not min_val_tree:is_empty(T2)).
+
+split_tree_general_case() ->
+    Pairs =
+        [
+            {a,5},
+            {b,3},
+            {d,2},
+            {c,7}
+        ],
+    T1 = store_pairs(Pairs),
+    {L,T2} = min_val_tree:split(2,T1),
+    ?assertEqual([{d,2},{b,3}],L),
+    ?assertEqual({ok,5},min_val_tree:min(T2)).
 
 %% ===========================================================================
 %% HELPERS
@@ -119,7 +179,7 @@ remove_keys(ToRemove,T) ->
 
 store_pairs_and_test_min(Pairs,ExpectedMin) ->
     T1 = store_pairs(Pairs),
-    Min = min_val_tree:get_min(T1),
+    Min = min_val_tree:min(T1),
     ?assertEqual(Min,ExpectedMin).
 
 
