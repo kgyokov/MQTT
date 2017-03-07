c629a5e42638b6d6c40327ae96318344f17c9d40
Kalin <kalin.gyokov>
2017-03-06 18:05:42 -0800

    versioned_set cleanup

--- src/versioned_set.erl
+++ src/versioned_set.erl
@@ -13,7 +13,7 @@
 -author("Kalin").
 
 %% API
--export([new/0, new/2, append/4, get_at/2, iterator_from/3, truncate/2, size/1, remove/3, take/2, next/1, to_list/1, update/3]).
+-export([new/2, get_at/2, truncate/2, size/1, update/3]).
 
 -export_type([set/1]).
 
@@ -24,66 +24,23 @@
 -opaque set(Struct)::#s_set{log:: gb_trees:tree(neg_integer()|0,Struct)}.
 -opaque(iter()::gb_trees:iter()|nil).
 
--spec(new() -> set(_)).
-new() -> new(0,gb_trees:empty()).
-
 -spec(new(non_neg_integer(),Struct) -> set(Struct)).
-new(StartVer,Tree) ->
-    #s_set{log = gb_trees:insert(-StartVer,Tree,gb_trees:empty())}.
-
--spec(append(any(),any(),non_neg_integer(), set(Struct)) -> set(Struct)).
-append(Key,Val,Ver,Set) ->
-    UpdateFun = fun(Last) -> gb_trees:enter(Key,Val,Last) end,
-    add_new_version(Ver,Set,UpdateFun).
-
-remove(Key, Ver,Set) ->
-    UpdateFun = fun(Last) -> gb_trees:delete_any(Key,Last) end,
-    add_new_version(Ver,Set,UpdateFun).
+new(StartVer,Struct) ->
+    #s_set{log = gb_trees:insert(-StartVer,Struct,gb_trees:empty())}.
 
 -spec update(non_neg_integer(),fun((Struct) -> Struct),set(Struct)) -> set(Struct).
 update(Ver,UpdateFun,Set) ->
     add_new_version(Ver,Set,UpdateFun).
 
--spec get_at(non_neg_integer(),set(_)) -> gb_trees:iter().
-get_at(Ver,Set) when Ver >= 0 ->
-    iterator_from(Ver,0,Set).
-
--spec iterator_from(non_neg_integer(),non_neg_integer(),set(_)) -> gb_trees:iter().
-iterator_from(Ver,Offset,Set) when Ver >= 0 ->
-    TreeAtVer = get_tree_at(Ver,Set),
-    gb_trees:iterator_from(Offset,TreeAtVer).
-
-%% ===========================================================================
-%%
-%%
-%% Iterator operations.
-%% @todo: Move to_list, next and take to a generic lazy lists implementation
-%%
-%% ===========================================================================
-
--spec to_list(iter()) -> [].
-to_list(Iter) ->
-    to_list(Iter,[]).
-
-to_list(Iter,Acc) ->
-    case next(Iter) of
-        {_,Val,Iter1} -> to_list(Iter1,[Val|Acc]);
-        none -> Acc
-    end.
-
-next(Iter) -> gb_trees:next(Iter).
-
--spec take(non_neg_integer(),iter()) -> {[any()],iter()}.
-take(Num,Iter) when Num >= 0 -> take(Num,Iter,[]).
-
-take(0,Iter,Acc)   -> {Acc,Iter};
-take(_Num,nil,Acc) -> {Acc,nil};
-take(Num,Iter,Acc) ->
+-spec get_at(non_neg_integer(),set(Struct)) -> gb_trees:tree(Struct).
+get_at(Ver,#s_set{log = Log}) ->
+    Iter = gb_trees:iterator_from(-Ver,Log), %% the whole reason for storing negative Sequence numbers
     case gb_trees:next(Iter) of
-        none -> {Acc,nil};
-        {_,Val,Iter1} -> take(Num-1,Iter1,[Val|Acc])
+        none ->
+            {_,LastVal} = gb_trees:largest(Log),
+            LastVal;
+        {_,Val,_} -> Val
     end.
-
 
 %% ===========================================================================
 %% Iterator operations.
@@ -97,16 +54,6 @@ add_new_version(Ver,Set = #s_set{log = Log},Fun) when is_integer(Ver), Ver >= 0 
     end,
     Next = Fun(Last),
     Set#s_set{log = gb_trees:insert(-Ver,Next,Log)}.
-
--spec get_tree_at(non_neg_integer(),set(Struct)) -> gb_trees:tree(Struct).
-get_tree_at(Ver,#s_set{log = Log}) ->
-    Iter = gb_trees:iterator_from(-Ver,Log), %% the whole reason for storing negative Sequence numbers
-    case gb_trees:next(Iter) of
-        none ->
-            {_,LastVal} = gb_trees:smallest(Log),
-            LastVal;
-        {_,Val,_} -> Val
-    end.
 
 -spec(truncate(non_neg_integer(), set(Struct)) -> set(Struct)).
 truncate(Ver,Set = #s_set{log = Log}) ->
--- test/versioned_set_test.erl
+++ test/versioned_set_test.erl
@@ -11,116 +11,77 @@
 
 -include("test_utils.hrl").
 
-setup_versioned_set() ->
-    versioned_set:new().
-
 all_test_() ->
         [
-            fun empty_after_creation/0,
-            fun error_when_accessing_at_0_index/0,
-            fun empty_result_when_accessing_at_less_than_smallest_index/0,
+            fun default_result_when_accessing_at_0_index/0,
+            fun default_result_when_accessing_at_less_than_smallest_index/0,
             fun correct_result_when_accessing_at_smallest_index/0,
             fun correct_result_when_accessing_at_largest_index/0,
             fun correct_result_when_accessing_above_largest_index/0,
-            fun truncate_below_minimum_leaves_set_intact/0,
+            fun update_below_minimum_leaves_set_intact/0,
             fun truncate_at_maximum_leaves_last_element/0,
             fun truncate_above_maximum_leaves_last_element/0,
-            fun correct_result_when_truncating/0,
-            fun remove_erases_the_key_from_corresponding_version/0,
-            fun removing_same_key_twice/0
+            fun correct_result_when_truncating/0
         ].
 
 
-empty_after_creation() ->
-    N = versioned_set:new(),
-    ?assertEqual(0,length((get_list_at(1,N)))),
-    ?assertEqual(0,length(get_list_at(2,N))).
+default_result_when_accessing_at_0_index() ->
+    N = versioned_set:new(0,val0),
+    ?assertEqual(val0,versioned_set:get_at(0,N)).
 
-error_when_accessing_at_0_index() ->
-    N = versioned_set:new(),
-    ?assertEqual(0,length(get_list_at(0,N))).
-
-empty_result_when_accessing_at_less_than_smallest_index() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key1,val1,2,N),
-    ?assertEqual(0,length(get_list_at(1,N1))).
+default_result_when_accessing_at_less_than_smallest_index() ->
+    N = versioned_set:new(2,val0),
+    N1 = versioned_set:update(3,fun(_) -> val1 end, N),
+    ?assertEqual(val0,versioned_set:get_at(1,N1)).
 
 correct_result_when_accessing_at_smallest_index() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
-    ValAt2 = get_list_at(2,N2),
-    ?assertEqual(1,length(ValAt2)),
-    ?lists_are_equal([val2],ValAt2).
+    N = versioned_set:new(2,val0),
+    N1 = versioned_set:update(3,fun(_) -> val3 end,N),
+    ?assertEqual(val0,versioned_set:get_at(2,N1)).
 
 correct_result_when_accessing_at_largest_index() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
-    ValAt3 = get_list_at(3,N2),
-    ?assertEqual(2,length(ValAt3)),
-    ?lists_are_equal([val2,val3],ValAt3).
+    N = versioned_set:new(0,val0),
+    N1 = versioned_set:update(2,fun(_) -> val2 end,N),
+    N2 = versioned_set:update(3,fun(_) -> val3 end,N1),
+    ?assertEqual(val3,versioned_set:get_at(3,N2)).
 
 correct_result_when_accessing_above_largest_index() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
-    ValAt3 = get_list_at(4,N2),
-    ?assertEqual(2,length(ValAt3)),
-    ?lists_are_equal([val2,val3],ValAt3).
+    N = versioned_set:new(0,val0),
+    N1 = versioned_set:update(2,fun(_) -> val2 end,N),
+    N2 = versioned_set:update(3,fun(_) -> val3 end,N1),
+    ?assertEqual(val3,versioned_set:get_at(4,N2)).
 
-truncate_below_minimum_leaves_set_intact() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
-    Truncated = versioned_set:truncate(1,N2),
+update_below_minimum_leaves_set_intact() ->
+    N = versioned_set:new(2,val0),
+    N1 = versioned_set:update(3,fun(_) -> val2 end,N),
+    Truncated = versioned_set:truncate(1,N1),
     ?assertEqual(2, versioned_set:size(Truncated)).
 
 truncate_above_maximum_leaves_last_element() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
+    N = versioned_set:new(0,val0),
+    N1 = versioned_set:update(2,fun(_) -> val2 end,N),
+    N2 = versioned_set:update(3,fun(_) -> val3 end,N1),
     Truncated = versioned_set:truncate(4,N2),
     ?assertEqual(1, versioned_set:size(Truncated)),
-    ?lists_are_equal([val2,val3], get_list_at(4,Truncated)).
+    ?assertEqual(val3, versioned_set:get_at(4,Truncated)).
 
 truncate_at_maximum_leaves_last_element() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
+    N = versioned_set:new(0,val0),
+    N1 = versioned_set:update(2,fun(_) -> val2 end,N),
+    N2 = versioned_set:update(3,fun(_) -> val3 end,N1),
     Truncated = versioned_set:truncate(3,N2),
     ?assertEqual(1, versioned_set:size(Truncated)),
-    ?lists_are_equal([val2,val3], get_list_at(3,Truncated)).
+    ?assertEqual(val3, versioned_set:get_at(4,Truncated)).
 
 correct_result_when_truncating() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
-    N3 = versioned_set:append(key4,val4,4,N2),
+    N = versioned_set:new(0,val0),
+    N1 = versioned_set:update(2,fun(_) -> val2 end,N),
+    N2 = versioned_set:update(3,fun(_) -> val3 end,N1),
+    N3 = versioned_set:update(4,fun(_) -> val4 end,N2),
     Truncated = versioned_set:truncate(3,N3),
     ?assertEqual(2, versioned_set:size(Truncated)),
-    ?lists_are_equal([val2,val3,val4], get_list_at(4,Truncated)).
+    ?assertEqual(val4, versioned_set:get_at(4,Truncated)).
 
-remove_erases_the_key_from_corresponding_version() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
-    N3 = versioned_set:append(key4,val4,4,N2),
-    N4 = versioned_set:remove(key3,5,N3),
-    ?lists_are_equal([val2,val4], get_list_at(5,N4)),
-    ?lists_are_equal([val2,val3,val4], get_list_at(4,N3)).
 
-removing_same_key_twice() ->
-    N = versioned_set:new(),
-    N1 = versioned_set:append(key2,val2,2,N),
-    N2 = versioned_set:append(key3,val3,3,N1),
-    N3 = versioned_set:append(key4,val4,4,N2),
-    N4 = versioned_set:remove(key3,5,N3),
-    N5 = versioned_set:remove(key3,6,N4),
-    ?lists_are_equal([val2,val4], get_list_at(6,N5)).
-
-get_list_at(Ver,Set) ->
-    Iter = versioned_set:get_at(Ver,Set),
-    versioned_set:to_list(Iter).
 
 
