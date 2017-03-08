368e207034826afdf867b9c0251d761d7731d6a2
Kalin <kalin.gyokov>
2017-03-07 18:54:14 -0800

    Moving test packages into an include file

--- test/shared_queue_test.erl
+++ test/shared_queue_test.erl
@@ -10,56 +10,7 @@
 -author("Kalin").
 
 -include_lib("eunit/include/eunit.hrl").
--include("mqtt_internal_msgs.hrl").
-
--define(QOS_0_PACKET_TOPIC1,#packet{
-    content = <<"Test">>,
-    qos = ?QOS_0,
-    retain = false,
-    seq = 1,
-    topic = <<"Topic1">>}).
-
--define(QOS_0_PACKET_TOPIC2,#packet{
-    content = <<"Test">>,
-    qos = ?QOS_0,
-    retain = false,
-    seq = 1,
-    topic = <<"Topic2">>}).
-
--define(QOS_1_PACKET_TOPIC1,#packet{
-    content = <<"Test">>,
-    qos = ?QOS_1,
-    retain = false,
-    seq = 1,
-    topic = <<"Topic1">>}).
-
--define(RETAINED_PACKET_TOPIC1,#packet{
-    content = <<"Test">>,
-    qos = ?QOS_0,
-    retain = true,
-    seq = 1,
-    topic = <<"Topic1">>}).
-
--define(RETAINED_PACKET2_TOPIC1,#packet{
-    content = <<"Test2">>,
-    qos = ?QOS_0,
-    retain = true,
-    seq = 1,
-    topic = <<"Topic1">>}).
-
--define(RETAINED_PACKET_TOPIC2,#packet{
-    content = <<"Test">>,
-    qos = ?QOS_0,
-    retain = true,
-    seq = 1,
-    topic = <<"Topic2">>}).
-
--define(RETAINED_PACKET_CLEAR_TOPIC1,#packet{
-    content = <<>>,
-    qos = ?QOS_0,
-    retain = true,
-    seq = 1,
-    topic = <<"Topic1">>}).
+-include("test_packages.hrl").
 
 no_packets_test() ->
     SQ = shared_queue:new(),
